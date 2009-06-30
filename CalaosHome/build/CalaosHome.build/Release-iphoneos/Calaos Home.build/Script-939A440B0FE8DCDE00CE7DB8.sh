1792fcad5c8f078f44a2c1a4486010d2a48a2be9
Raoul Hecky <raoul@calaos.fr>
2009-06-30 10:00:03 +0000

    git-svn-id: http://dev.calaos.fr/calaos-svn/calaos_bootsplash@1491 5480cf3d-c827-0410-bd00-b44130fca4db

--- CalaosHome/build/CalaosHome.build/Release-iphoneos/Calaos Home.build/Script-939A440B0FE8DCDE00CE7DB8.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-export CODESIGN_ALLOCATE=/Developer/Platforms/iPhoneOS.platform/Developer/usr/bin/codesign_allocate
-if [ "${PLATFORM_NAME}" == "iphoneos" ]; then
-	$HOME/gen_entitlements.py "com.calaos.${PROJECT_NAME}" "${BUILT_PRODUCTS_DIR}/${WRAPPER_NAME}/${PROJECT_NAME}.xcent";
-	codesign -f -s "iPhone developer" --resource-rules "${BUILT_PRODUCTS_DIR}/${WRAPPER_NAME}/ResourceRules.plist" \
-		 --entitlements "${BUILT_PRODUCTS_DIR}/${WRAPPER_NAME}/${PROJECT_NAME}.xcent"  "${BUILT_PRODUCTS_DIR}/${WRAPPER_NAME}/"
-fi
